8ed1b040bc60def2314edc210b15df8c33fcc430
Austin Almond <aust.almond@gmail.com>
2025-03-21 19:01:52 -0400

    Print debug in _process

--- src/gdclips.cpp
+++ src/gdclips.cpp
@@ -1,8 +1,13 @@
 #include "gdclips.h"
 #include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
 
 using namespace godot;
 
 void GDClips::_bind_methods() {
 
+}
+
+void GDClips::_process(double delta) {
+    godot::UtilityFunctions::print("clips!");
 }
