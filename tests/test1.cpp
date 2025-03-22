f8122c7153f00ccdf9b6649ea6084a9e4d1f75c1
Austin Almond <aust.almond@gmail.com>
2025-03-22 14:49:23 -0400

    Basic googletest assertion

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,5 +1,9 @@
 #include "gtest/gtest.h"
 
+TEST(Test1, BasicAssertions) {
+    EXPECT_TRUE(true);
+}
+
 int main(int nArgs, char** vArgs) {
     ::testing::InitGoogleTest(&nArgs, vArgs);
     return RUN_ALL_TESTS();
