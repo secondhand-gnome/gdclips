81fdc5a8c297ac8a6056cf02731bf2bd741fc030
Austin Almond <aust.almond@gmail.com>
2025-03-22 22:59:54 -0400

    UnmakeInstance

--- src/ClipsInstance.cpp
+++ src/ClipsInstance.cpp
@@ -10,4 +10,25 @@ void godot::ClipsInstance::set_instance(Instance *p_instance) {
 
 godot::ClipsInstance::~ClipsInstance() {
     ReleaseInstance(instance);
+
+    UnmakeInstanceError err = UnmakeInstance(instance);
+    switch (err) {
+        case UIE_NO_ERROR:
+            // No error
+            break;
+        case UIE_NULL_POINTER_ERROR:
+            godot::UtilityFunctions::push_error("[ClipsInstance.destructor] The i parameter was NULL.");
+            break;
+        case UIE_COULD_NOT_DELETE_ERROR:
+            godot::UtilityFunctions::push_error(
+                "[ClipsInstance.destructor] The instance could not be deleted (such as when pattern matching of a fact or instance is already occurring).");
+            break;
+        case UIE_DELETED_ERROR:
+            godot::UtilityFunctions::push_error("[ClipsInstance.destructor] THe instance was already deleted.");
+            break;
+        case UIE_RULE_NETWORK_ERROR:
+            godot::UtilityFunctions::push_error(
+                "[ClipsInstance.destructor] An error occured while the deletion was being processed in the rule network.");
+            break;
+    }
 }
