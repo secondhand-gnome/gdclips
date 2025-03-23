1b0fb49fb21ee696887317235c288e5c520cb1cd
Austin Almond <aust.almond@gmail.com>
2025-03-22 22:56:09 -0400

    Handle retract errors

--- src/ClipsFact.cpp
+++ src/ClipsFact.cpp
@@ -16,5 +16,22 @@ void godot::ClipsFact::set_fact(Fact *p_fact) {
 
 godot::ClipsFact::~ClipsFact() {
     ReleaseFact(fact);
-    Retract(fact);
+
+    RetractError err = Retract(fact);
+    switch (err) {
+        case RE_NO_ERROR:
+            // No error
+            break;
+        case RE_NULL_POINTER_ERROR:
+            godot::UtilityFunctions::push_error("[ClipsFact.destructor] The f parameter was NULL.");
+            break;
+        case RE_COULD_NOT_RETRACT_ERROR:
+            godot::UtilityFunctions::push_error(
+                "[ClipsFact.destructor] The fact could not be retracted (such as when pattern matching of a fact or instance is already occurring)");
+            break;
+        case RE_RULE_NETWORK_ERROR:
+            godot::UtilityFunctions::push_error(
+                "[ClipsFact.destructor] An error occurs while the retraction was being processed in the rule network.");
+            break;
+    }
 }
