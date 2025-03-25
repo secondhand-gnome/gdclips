670e49898fd29fa1baf822df22576db475642990
Austin Almond <aust.almond@gmail.com>
2025-03-24 23:17:52 -0400

    CLIPS unit tests

--- demo/test/clips/test_clips_env.gd
+++ demo/test/clips/test_clips_env.gd
@@ -26,3 +26,37 @@ func test_debugging():
 	var fact: ClipsFact = env.clips_assert_string("(list (numbers 61 31 27 48))")
 	assert_eq(env.clips_run(-1), 4)
 	assert_true(env.clips_dribble_off())
+
+# 4.5.1 Debugging Revisited
+func test_debugging_revisited():
+	assert_true(env.clips_build("
+		(deftemplate list
+			(multislot numbers))"))
+			
+	assert_true(env.clips_build("
+		(defrule sort
+			?f <- (list (numbers $?b ?x ?y&:(> ?x ?y) $?e))
+			=>
+			(modify ?f (numbers ?b ?y ?x ?e)))"))
+	
+	# TODO - some of these throw syntax errors but it's unclear why
+	env.clips_eval("(watch facts)")
+	env.clips_eval("(watch rules)")
+	env.clips_eval("(watch activations)")
+	env.clips_eval("(dribble-on sort.dbg)")
+	env.clips_eval("(reset)")
+	env.clips_eval("(assert (list (numbers 61 31 27 48)))")
+	env.clips_eval("(run)")
+	env.clips_eval("(dribble-off)")
+
+# 4.5.2 String Builder Function Call
+func test_string_builder_function_call():
+	var sb: ClipsStringBuilder = env.clips_create_string_builder(512)
+	
+	sb.sb_append("John")
+	sb.sb_append(" ")
+	sb.sb_append("Doe")
+	
+	assert_eq(sb.sb_get_contents(), "John Doe")
+	
+	# TODO evaluate (upcase) function here, get CV returned from eval
